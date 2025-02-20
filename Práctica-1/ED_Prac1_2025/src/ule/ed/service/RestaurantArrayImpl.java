8ce2edc70705b7cf5e0b8e040d86d5a798f59b39
Martín Rebollo <mrebog03@estudiantes.unileon.es>
2025-02-20 14:10:17 +0100

    Práctica 1 avanzada + Tests hechos de lo que está avanzado

--- Práctica-1/ED_Prac1_2025/src/ule/ed/service/RestaurantArrayImpl.java
+++ Práctica-1/ED_Prac1_2025/src/ule/ed/service/RestaurantArrayImpl.java
@@ -22,16 +22,20 @@ public class RestaurantArrayImpl implements IRestaurant {
 
 	public RestaurantArrayImpl(String name, int nTables,int aforoMax, int discount){ 
 		// Debe crear el array de mesas con todas las posiciones a null
+		this.name = name;
+		this.nTables = nTables;
+		this.maxCapacity = aforoMax;
+		this.discount = discount;
+		this.nClients = 0;
 		
-	
-		
+		this.tables = new Service[this.nTables];
 	}
 
 
 
 	@Override
 	public String getName() {
-		return "";
+		return this.name;
 		
 	}
 
@@ -39,50 +43,73 @@ public class RestaurantArrayImpl implements IRestaurant {
 
 	@Override
 	public int getMaxCapacity() {
-		return 0;
+		return maxCapacity;
 	}
 
 
 
 	@Override
 	public int getNumberOfChildren() {
-		return 0;
+		int numberOfChildren = 0;
+		for(int i = 0; i < nTables; i++) {
+			if(tables[i] != null) {
+			numberOfChildren += tables[i].getNChildren();
+			}
+		}
+		return numberOfChildren;
 	}
 
 
 
 	@Override
 	public int getNumberOfPeople() {
-		return 0;
+		return nClients;
 	}
+
 
 
 
 	@Override
 	public int getActualCapacity() {
-		return 0;
+		return this.maxCapacity - getNumberOfPeople();
 	}
 
 
 
 	@Override
 	public int getNumberTablesOccupied() {
+		int tablesOccupied = 0;
 		
-		return 0;
+		for (int i = 0; i < nTables; i++) {
+			if (tables[i] != null) {
+				tablesOccupied++;
+			}
+		}
+		
+		return tablesOccupied;
 	}
 
 
 
 	@Override
 	public int getNumberOfEmptyTables() {
-		return 0;
+		return this.nTables - getNumberTablesOccupied();
 	}
 
 
 
 	@Override
 	public int getNumberOfTablesWithChildren() {
-		return 0;
+		int tablesWithChildren = 0;
+		
+		for (int i = 0; i < nTables; i++) {
+			if (tables[i] != null) {
+				if(tables[i].getNChildren() > 0){
+				tablesWithChildren++;
+				}
+			}
+		}
+		return tablesWithChildren;
 	}
 
 
@@ -98,48 +125,78 @@ public class RestaurantArrayImpl implements IRestaurant {
 
 	@Override
 	public Service getService(int ntable) {
-		return null;
+		return this.tables[ntable - 1];
 	}
 
 
 
 	@Override
 	public void addDishToTable(int nTable, String name, double price, int count) {
-	
+		if (tables[nTable - 1] != null) {
+			tables[nTable - 1].addDishToTable(name, price, count);
+		} else {
+			throw new IllegalArgumentException("La mesa no está ocupada");
+		}
 	}
 
 
 
 	@Override
 	public double getFinalPrice(int ntable) {
-		return 0.0;
+		double finalPrice = 0.0;
+		tables[ntable - 1].getOrder();
+		for (int i = 0; i < tables[ntable - 1].getOrder().size(); i++) {
+			finalPrice += (tables[ntable - 1].getOrder().get(i).getPrice() * tables[ntable - 1].getOrder().get(i).getCount());
+		}
+		return finalPrice - (finalPrice * discount / 100);
 	}
 
 
 
 	@Override
 	public double getFinalPriceRestaurant() {
-		return 0.0;
+		double finalPrice = 0.0;
+		for(int i = 0; i < nTables; i++) {
+			if(tables[i] != null) {
+				finalPrice += getFinalPrice(i + 1);
+			}
+		}
+		return finalPrice;
     }
 
 
 
 	@Override
 	public boolean emptyTable(int nTable) {
-	return false;
+		if (tables[nTable - 1] != null) {
+			return false;
+		}
+	return true;
 	}
 
 
 
 	@Override
 	public int occupyTable(int nPeople, int nChildren) {
-		return 0;
+		for(int i = 0; i < this.nTables; i++) {
+			if(tables[i] == null) {
+				tables[i] = new Service(nPeople, nChildren);
+				nClients += nPeople;
+				return i + 1;
+			}
+		}
+		return -1;
 	}
 	
 	@Override
 	public boolean occupyTable(int nTable, int nPeople, int nChildren) {
+		if(tables[nTable] != null) {
+			return true;
+		} else {
+			tables[nTable] = new Service(nPeople, nChildren);
+			nClients += nPeople;
+		}
 		return false;
-		
 	}
 	
 }
--- Práctica-1/ED_Prac1_2025/src/ule/ed/service/RestaurantArrayImplTests.java
+++ Práctica-1/ED_Prac1_2025/src/ule/ed/service/RestaurantArrayImplTests.java
@@ -1,5 +1,8 @@
 package ule.ed.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.*;
 
 
@@ -53,5 +56,120 @@ public class RestaurantArrayImplTests {
 	    Assert.assertEquals(27.0,res.getFinalPriceRestaurant(),0.0);    
 	}
 	
+	//**************************************************************************
+	@Test
+	public void testGetNumberOfPeople() throws Exception{
+            Assert.assertEquals(1,res.occupyTable(4, 2)); // le asigna la mesa 1
+            Assert.assertEquals(4,res.getNumberOfPeople());
+	}
+	
+	@Test
+	public void testGetNumberOfChildren() throws Exception {
+		Assert.assertEquals(1, res.occupyTable(4, 2)); // le asigna la mesa 1
+		Assert.assertEquals(2, res.getNumberOfChildren());
+	}
+	
+	@Test
+	public void testGetName() throws Exception{
+		Assert.assertEquals("Casa Pepe", res.getName());
+	}
+	
+	@Test
+	public void testGetNumberTablesOccupied() throws Exception {
+		Assert.assertEquals(0, res.getNumberTablesOccupied());
+		res.occupyTable(4, 0);
+		Assert.assertEquals(1, res.getNumberTablesOccupied());
+	}
+	
+	@Test
+	public void testNumberFreeTables() throws Exception{
+		Assert.assertEquals(15, res.getNumberOfEmptyTables());
+		res.occupyTable(4, 0);
+		Assert.assertEquals(14, res.getNumberOfEmptyTables());
+	}
+	
+	@Test
+	public void testTablesWithChildrens() throws Exception{
+		Assert.assertEquals(0, res.getNumberOfTablesWithChildren());
+        res.occupyTable(4, 2);
+        Assert.assertEquals(1, res.getNumberOfTablesWithChildren());
+        res.occupyTable(4, 0);
+        Assert.assertEquals(1, res.getNumberOfTablesWithChildren());
+	}
+	
+	@Test
+	public void testAddDishToTable() throws Exception {
+		res.occupyTable(4, 2);
+		res.addDishToTable(1, "Arroz", 10.0, 2);
+		Assert.assertEquals(18.0, res.getFinalPrice(1), 0.0);
+	}
+	
+	@Test (expected = Exception.class)
+	public void testAddDishToNotExistingTable() throws Exception{
+		res.addDishToTable(2, "Arroz", 10.0, 2);
+	}
+	
+	@Test
+	public void testEmptyTable() throws Exception {
+		res.emptyTable(1);
+	}
+	
+	@Test
+	public void testNotEmptyTable() throws Exception {
+		res.occupyTable(4, 2);
+		res.occupyTable(4, 0);
+		res.emptyTable(2);
+	}
+	
+	@Test
+	public void testMaxOccupiedTable() throws Exception {
+		for (int i = 0; i < 15; i++) {
+			Assert.assertEquals(i + 1, res.occupyTable(4, 2));
+		}
+	}
+	
+	@Test
+	public void testOccupiedTable() throws Exception {
+		for (int i = 0; i < 15; i++) {
+			res.occupyTable(4, 2);
+		}
+		Assert.assertEquals(-1, res.occupyTable(4, 2));
+	}
+	
+	@Test
+	public void testMaxOccupiedTableSelected() throws Exception {
+		Assert.assertEquals(false, res.occupyTable(5, 10, 2));
+	}
+	
+	@Test
+	public void testOccupiedTableSelected() throws Exception {
+		Assert.assertEquals(false, res.occupyTable(5, 10, 2));
+		res.occupyTable(5, 10, 2);
+		Assert.assertEquals(true, res.occupyTable(5, 10, 2));
+
+	}
+
+
+    @Test
+    public void testGetNumbersOfEmptyTablesWhenFull() {
+        res.occupyTable(4, 2); // Mesa 1
+        res.occupyTable(3, 1); // Mesa 2
+        res.occupyTable(2, 1); // Mesa 3
+        res.occupyTable(5, 0); // Mesa 4
+        res.occupyTable(6, 3); // Mesa 5
+
+        Assert.assertTrue(res.getNumbersOfEmptyTables().isEmpty());
+    }
+
+    @Test
+    public void testGetServiceExistingTable() {
+        res.occupyTable(4, 2); // Ocupa la mesa 1
+        Assert.assertEquals("{Servicio:4personas,2ninyos, total= 0.0}", res.getService(1).toString());
+    }
+
+    @Test
+    public void testGetServiceNonExistingTable() {
+    	Assert.assertNull(res.getService(1)); // No hay servicio en la mesa 1
+    }
 }
 
